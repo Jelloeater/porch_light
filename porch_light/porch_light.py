59a6ea4e25efff98d136cae8c1c224e8e49d9791
Jesse Schoepfer <jelloeater@gmail.com>
2022-12-25 01:32:04 -0500

    Initial Commit

---
+++ porch_light/porch_light.py
@@ -0,0 +1,19 @@
+import logging
+
+# syslog_handler = logging.handlers.SysLogHandler(address=(settings.syslog_server, settings.syslog_port))
+console_handler = logging.StreamHandler()
+# syslog_handler.setFormatter(logging.Formatter("APP_NAME \n[%(asctime)s] [%(levelname)8s] --- %(message)s (%(filename)s:%(funcName)s():%(lineno)s)"))
+console_handler.setFormatter(
+    logging.Formatter("[%(asctime)s] [%(levelname)8s] --- %(message)s (%(filename)s:%(funcName)s():%(lineno)s)"))
+
+# logging.basicConfig(level=logging.WARN, handlers=[console_handler, syslog_handler])
+logging.basicConfig(level=logging.DEBUG, handlers=[console_handler])
+
+
+class Main:
+    def start(self):
+        logging.debug("SoP")
+
+
+if __name__ == '__main__':
+    Main.start()
---
+++ test/test_porch_light.py
@@ -0,0 +1,5 @@
+import porch_light.porch_light
+
+
+def test_main():
+    porch_light.porch_light.Main().start()
