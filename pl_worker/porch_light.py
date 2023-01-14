3c1226e8f3ed3ba72e3398c215c89a1fb2cc5e32
Jesse Schoepfer <jelloeater@gmail.com>
2023-01-14 17:05:19 -0500

    Updating unit test

--- pl_worker/porch_light.py
+++ pl_worker/porch_light.py
@@ -1,9 +1,13 @@
 import logging.handlers
+
 console_handler = logging.StreamHandler()
-console_handler.setFormatter(logging.Formatter("[%(asctime)s] [%(levelname)8s] --- %(message)s (%(filename)s:%(funcName)s():%(lineno)s)"))
+console_handler.setFormatter(
+    logging.Formatter(
+        "[%(asctime)s] [%(levelname)8s] --- %(message)s (%(filename)s:%(funcName)s():%(lineno)s)"
+    )
+)
 logging.basicConfig(level=logging.DEBUG, handlers=[console_handler])
 
 
-
-if __name__ == '__main__':
-    logging.info('SoP')
+if __name__ == "__main__":
+    logging.info("SoP")
--- test/test_main.py
+++ test/test_main.py
@@ -1,6 +1,24 @@
-import pytest
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
+host_env = os.getenv("HUBITAT_HOST")
+token_env = os.getenv("HUBITAT_API_TOKEN")
+app_id_env = os.getenv("HUBITAT_API_APP_ID")
 
-import hubitatcontrol
+import hubitatcontrol as Hubitat
+
+test_device_name = "Porch"
+
+
+def test_device_bulb():
+    h = Hubitat.Hub(host=host_env, token=token_env, app_id=app_id_env)
+    d = h.get_device(test_device_name)
+    test_bulb = Hubitat.Advanced_Zigbee_RGBW_Bulb(h, d)
+
+    test_bulb.turn_on()
+    assert test_bulb.switch == "on"
+    test_bulb.turn_off()
+    assert test_bulb.switch == "off"
+    test_bulb.turn_on()
+    assert test_bulb.switch == "on"
