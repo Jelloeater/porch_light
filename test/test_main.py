d83bb99d7e48fb26c1415c04c3edf62c4ad076ed
Jesse Schoepfer <jelloeater@gmail.com>
2023-02-04 17:26:17 -0500

    Added unit test

--- test/test_main.py
+++ test/test_main.py
@@ -1,13 +1,18 @@
 from dotenv import load_dotenv
 import pl_worker.porch_light
 import pl_worker.webserver
+from fastapi.testclient import TestClient
 
 load_dotenv()
+# Webserver test client
+client = TestClient(pl_worker.webserver.app)
 
 
 def test_check_hub():
     assert pl_worker.porch_light.check_hub().devices is not None
 
 
-# def test_webserver():
-#     pl_worker.webserver.start_server()
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
